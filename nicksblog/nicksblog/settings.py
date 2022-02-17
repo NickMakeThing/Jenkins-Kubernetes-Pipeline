a905a9978cfab4e37602461ef6f9ab6c42ce5c4d
NickMakeThing <nicksegalwork@gmail.com>
2022-02-18 07:45:24 +1100

    turned off debug mode. configured static file serving for prod.

--- nicksblog/nicksblog/settings.py
+++ nicksblog/nicksblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGOKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -67,9 +67,6 @@ TEMPLATES = [
         },
     },
 ]
-
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 WSGI_APPLICATION = 'nicksblog.wsgi.application'
 
@@ -119,10 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'media')
+    os.path.join(BASE_DIR,'media'),
 ]
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
