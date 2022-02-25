b52fd4d60bb158fe3aab931e9e190beb9c4d47cd
NickMakeThing <nicksegalwork@gmail.com>
2022-02-25 06:38:57 +0000

    testing jenkins,webhooks and pipeline

--- nicksblog/nicksblog/settings.py
+++ nicksblog/nicksblog/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-#adding this to test jenkins ....
+#adding this to test jenkins
+print('sdfdsfdsf')
 from pathlib import Path
 import os 
 
